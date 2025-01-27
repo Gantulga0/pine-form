b380963fc2dca04d0a520ea1612bd57a1033cc9b
24LP9558 24LP9558 <tudewdagwagantulga@gmail.com>
2025-01-27 09:07:21 +0800

    odoo effect nemne

--- src/components/StepThree.js
+++ src/components/StepThree.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import PineconeLogo from '@/icons/PineconeLogo';
 import { isStepThreeValid } from '@/utils/stepThreeValidation';
 
